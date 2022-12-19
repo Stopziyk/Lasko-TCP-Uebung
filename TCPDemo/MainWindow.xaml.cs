d0efb69f57470848ba6730a006bfbb1824eff764
Jannis Home-PC <JnsTrt@mailbox.org>
2022-12-19 16:42:14 +0100

    Projektdateien hinzufügen.

---
+++ TCPDemo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace TCPDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBox lb = new ListBox();
+            Content = lb;
+            FontSize = 21;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener server = 
+                new TcpListener(ip,8088);
+            server.Start();
+            MessageBox.Show("server started");
+            TcpClient client = 
+            server.AcceptTcpClient();
+            MessageBox.Show("new client");
+            StreamReader reader =
+                new StreamReader(client.GetStream());
+            string Nachricht = reader.ReadLine();
+            lb.Items.Add(Nachricht);
+        }
+    }
+}
---
+++ TCPDemoClient/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace TCPDemoClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "Client";
+            IPAddress serverip =
+                IPAddress.Parse("127.0.0.1");
+            TcpClient meClient =
+                new TcpClient("127.0.0.1", 8088);
+            StreamWriter writer =
+                new StreamWriter(
+                    meClient.GetStream());
+            writer.WriteLine("Moin!");
+            writer.Flush();
+        }
+    }
+}
